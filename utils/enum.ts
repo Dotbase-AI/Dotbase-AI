b936bc58e16e4f82790460be5ef67997abbca5da
Roni-26 <85412582+Roni-26@users.noreply.github.com>
2025-01-18 00:00:00 +0000

    Setup dashboard component layout

--- utils/enum.ts
+++ utils/enum.ts
@@ -1,7 +1,6 @@
 export enum OAIModelsEnum {
   GPT_4o = 'gpt-4o',
   GPT_4_32K = 'gpt-4-32k',
-  GPT_3_5_TURBO = 'gpt-3.5-turbo',
   GPT_3_5_TURBO_16K = 'gpt-3.5-turbo-16k',
 }
 
@@ -9,5 +8,4 @@ export enum AgentSelectionStrategyEnum {
   RR = 'round_robin',
   AUTO = 'auto',
   MANUAL = 'manual',
-  RANDOM = 'random',
 }
