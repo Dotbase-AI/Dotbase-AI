08ab6a3bf7454d13552246bd3de39e5ca8963724
Roni-26 <85412582+Roni-26@users.noreply.github.com>
2025-01-26 03:00:00 +0000

    Create dashboard library components

--- components/ui/animations/LoadingDots.tsx
+++ components/ui/animations/LoadingDots.tsx
@@ -1,9 +1,7 @@
 "use client";
-
 const LoadingDots = () => {
     return (
       <span className="inline-flex items-center">
-        <span className="animate-[loadingDot_1s_infinite] opacity-0">.</span>
         <span className="animate-[loadingDot_1s_0.2s_infinite] opacity-0">.</span>
         <span className="animate-[loadingDot_1s_0.4s_infinite] opacity-0">.</span>
       </span>
