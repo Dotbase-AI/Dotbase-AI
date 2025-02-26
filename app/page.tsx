4b4a5fadfb137063ae6534d62906d794409932a1
Roni-26 <85412582+Roni-26@users.noreply.github.com>
2025-02-26 20:00:00 +0000

    Create dashboard export functionality

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import Footer from "../components/home/Footer";
 import HeroSection from "@/components/home/HeroSection";
 import FeaturesSection from "@/components/home/FeaturesSection";
 import AgentsSection from "@/components/home/AgentsSection";
+import TokenomicsSection from "@/components/home/TokenomicsSection";
 import { Providers } from "@/providers/privy-provider";
 
 export default function Home() {
   return (
+        <Providers>
+          <main className="bg-black">
             <Header />
             <HeroSection />
             <AgentsSection />
